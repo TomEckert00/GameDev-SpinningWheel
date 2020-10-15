c17d6b57b7885b15b93459a7837d2530abcf9c86
TomEckert00 <toe00@web.de>
2020-10-15 19:46:13 +0200

    initiate background and wheel

---
+++ SpinningWheel/Assets/Wheel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    public Transform centerCoordinate;
+    public double radius;
+
+    void Awake()
+    {
+        centerCoordinate = new Transform()
+    }
+}
